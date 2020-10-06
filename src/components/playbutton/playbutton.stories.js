9a48962a664735e363c10aef51d0c3f0aaa0be24
ncbecker <nbecker.ac@gmail.com>
2020-10-06 15:07:03 +0200

    Clean up folder and file structure

--- src/components/playbutton/playbutton.stories.js
+++
@@ -1,6 +0,0 @@
-import "./playbutton.css";
-import playbutton from "./playbutton.html";
-
-export default { title: "Components/PlayButton" };
-
-export const gradient = () => playbutton;
--- src/components/sharebutton/sharebutton.stories.js
+++
@@ -1,6 +0,0 @@
-import "./sharebutton.css";
-import sharebutton from "./sharebutton.html";
-
-export default { title: "Components/ShareButton" };
-
-export const transparent = () => sharebutton;
