b15e907a4dd7ae5002b80dcb4ead85307d253c7a
ncbecker <nbecker.ac@gmail.com>
2020-10-11 22:40:39 +0200

    Refactor variables, storybook js and bottom-nav

--- src/components/nav/nav.stories.js
+++ src/components/nav/nav.stories.js
@@ -1,6 +1,6 @@
 import "./nav.css";
 import bottomnav from "./bottom-nav.html";
 
-export default { title: "Components/Nav" };
+export default { title: "Components/Nav", parameters: { layout: "centered" } };
 
 export const BottomNavigation = () => bottomnav;
--- src/components/typography/typography.stories.js
+++ src/components/typography/typography.stories.js
@@ -1,6 +1,9 @@
 import "./typography.css";
 import typography from "./typography.html";
 
-export default { title: "Components/Typography" };
+export default {
+  title: "Components/Typography",
+  parameters: { layout: "centered" },
+};
 
 export const headers = () => typography;
