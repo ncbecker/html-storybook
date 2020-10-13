dbafe51117f53765f17ff1af1cfd879375d77133
ncbecker <nbecker.ac@gmail.com>
2020-10-13 23:01:00 +0200

    Add object to parameter and onclick audio event

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -2,10 +2,7 @@ import "./track.css";
 import artistAvatarSrc from "../../assets/michaeljackson.png";
 import playActionSrc from "../../assets/playactionbutton.svg";
 
-export function createTrackElement(title, artist) {
-  // const artistName = "Michael Jackson";
-  // const trackTitle = "Billie Jean";
-
+export function createTrackElement(track) {
   const divTrack = document.createElement("div");
   divTrack.className = "track";
 
@@ -18,17 +15,20 @@ export function createTrackElement(title, artist) {
   const divButtonBox = document.createElement("div");
   divButtonBox.className = "track--button-box";
 
+  const divTrackBorder = document.createElement("div");
+  divTrackBorder.className = "track--border";
+
   const artistAvatar = document.createElement("img");
   artistAvatar.src = artistAvatarSrc;
-  artistAvatar.alt = `Artist Avatar ${artist}`;
+  artistAvatar.alt = `Artist Avatar ${track.artist}`;
   artistAvatar.className = "track__artist-avatar";
 
   const trackName = document.createElement("h3");
-  trackName.innerText = title;
+  trackName.innerText = track.title;
   trackName.className = "track__name";
 
   const trackArtist = document.createElement("span");
-  trackArtist.innerText = artist;
+  trackArtist.innerText = track.artist;
   trackArtist.className = "track__artist";
 
   const playActionButton = document.createElement("button");
@@ -38,15 +38,25 @@ export function createTrackElement(title, artist) {
   playActionIcon.alt = "Play Action Icon";
   playActionIcon.className = "track__action-button";
 
-  divTrack.append(divAvatarBox, divDescriptionBox, divButtonBox);
+  const playAudio = new Audio(track.audioSrc);
+  playActionButton.onclick = function () {
+    playAudio.play();
+  };
+
+  // playActionButton.onclick = function () {
+  //   playAudio.pause();
+  // };
+
+  divTrack.append(
+    divAvatarBox,
+    divDescriptionBox,
+    divButtonBox,
+    divTrackBorder
+  );
   divAvatarBox.append(artistAvatar);
   divDescriptionBox.append(trackName, trackArtist);
   divButtonBox.append(playActionButton);
   playActionButton.append(playActionIcon);
-
-  playActionButton.onclick = function () {
-    alert("CLICK EVENT");
-  };
 
   return divTrack;
 }
--- src/components/track/track.stories.js
+++ src/components/track/track.stories.js
@@ -5,7 +5,26 @@ export default {
   parameters: { layout: "centered" },
 };
 
-export const thriller = () => createTrackElement("Thriller", "Michael Jackson");
-export const beatIt = () => createTrackElement("Beat It", "Michael Jackson");
+export const thriller = () =>
+  createTrackElement({
+    title: "Thriller",
+    artist: "Michael Jackson",
+    audioSrc:
+      "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Planet_Terror_Records/Kingstux/The_Illusion_is_Worth_Fighting_For/Kingstux_-_04_-_Tetris_Trance.mp3",
+  });
+
+export const beatIt = () =>
+  createTrackElement({
+    title: "Beat It",
+    artist: "Michael Jackson",
+    audioSrc:
+      "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Planet_Terror_Records/Kingstux/The_Illusion_is_Worth_Fighting_For/Kingstux_-_04_-_Tetris_Trance.mp3",
+  });
+
 export const billieJean = () =>
-  createTrackElement("Billie Jean", "Michael Jackson");
+  createTrackElement({
+    title: "Billie Jean",
+    artist: "Michael Jackson",
+    audioSrc:
+      "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Planet_Terror_Records/Kingstux/The_Illusion_is_Worth_Fighting_For/Kingstux_-_04_-_Tetris_Trance.mp3",
+  });
